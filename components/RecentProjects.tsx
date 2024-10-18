030de21093586842e77fd1c7788c40cc0bcf525c
vishvam <vishvamaggarwal@gmail.com>
2024-10-18 21:49:19 +0530

    Sentry(report a bug) setup

--- components/RecentProjects.tsx
+++ components/RecentProjects.tsx
@@ -11,7 +11,7 @@ const PinContainer = dynamic(
 
 const RecentProjects = () => {
     return (
-        <div className='py-20'>
+        <div className='py-20' id="projects">
             <h1 className='heading'>
                 A small selection of {' '}
                 <span className='text-purple'>recent projects</span>
---
+++ sentry.server.config.ts
@@ -0,0 +1,15 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://45feb108bd2ec8d4f11aed34f7b9c016@o4508144305700864.ingest.us.sentry.io/4508144307929088",
+
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+  tracesSampleRate: 1,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+});
