28b303dc1b12f76c68436b4e562f95ec9df8aa0d
vishvam <vishvamaggarwal@gmail.com>
2024-10-19 03:59:58 +0530

    Final bug fixes(deployment)

--- components/ui/Globe.tsx
+++ components/ui/Globe.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 "use client";
 import { useEffect, useRef, useState } from "react";
 import { Color, Scene, Fog, PerspectiveCamera, Vector3 } from "three";
