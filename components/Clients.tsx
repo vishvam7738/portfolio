64a8f6361db0334a4a5ce47df068caf353dd16b9
vishvam <vishvamaggarwal@gmail.com>
2024-10-19 00:19:38 +0530

    Bug fixes in footer

---
+++ components/Clients.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { InfiniteMovingCards } from './ui/InfiniteMovingCards'
+
+const Clients = () => {
+    return (
+        <div className='py-20' id="projects">
+            <h1 className='heading'>
+                Let's{' '}
+                <span className='text-purple'> Connect!</span>
+            </h1>
+            <div className='flex flex-col items-center'>
+                
+            
+            </div>
+        </div>
+    )
+}
+
+export default Clients
